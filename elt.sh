9645fbe8179bc436e63abc2bc1a477f8668c2928
marcos314 <marcoswesley314@gmail.com>
2020-10-15 10:55:19 -0300

    Remoção de comentário

--- elt.sh
+++ elt.sh
@@ -1,8 +1,8 @@
-#!/bin/bash
+!/bin/bash
 
-# Pipeline 
-#chmod +x download_dados.sh
+Pipeline 
+chmod +x download_dados.sh
 
-#	./download_dados.sh
+	./download_dados.sh
 	python3 scripts/create_tables.py
 	python3 scripts/insert_data.py
